42f00689cd628fc8d3b93a5803b8b922805c3b79
HeroicYang <me@heroicyang.com>
2013-11-01 18:11:32 +0800

    增加 favorite_node、favorite_topic schema

---
+++ models/schemas/favorite_node.js
@@ -0,0 +1,48 @@
+/**
+ * FavoriteNodeSchema definition
+ * @author heroic
+ */
+
+/**
+ * Module dependencies
+ */
+var mongoose = require('mongoose'),
+  Schema = mongoose.Schema;
+
+/**
+ * Collection name in the database is `favorite_node`
+ * @type {Schema}
+ */
+var FavoriteNodeSchema = new Schema({
+  userId: {
+    type: String,
+    required: true
+  },
+  node: {
+    id: {
+      type: String,
+      require: true
+    },
+    name: {
+      type: String,
+      required: true
+    }
+  }
+}, {
+  collection: 'favorite_node'
+});
+
+/**
+ * Plugins
+ */
+FavoriteNodeSchema
+  .plugin(require('../mongoose_plugins/timestamp'));
+
+/**
+ * Exports schema and model name
+ * @type {object}
+ */
+module.exports = {
+  schema: FavoriteNodeSchema,
+  modelName: 'FavoriteNode'
+};
---
+++ models/schemas/favorite_topic.js
@@ -0,0 +1,42 @@
+/**
+ * FavoriteTopicSchema definition
+ * @author heroic
+ */
+
+/**
+ * Module dependencies
+ */
+var mongoose = require('mongoose'),
+  Schema = mongoose.Schema;
+
+/**
+ * Collection name in the database is `favorite_topic`
+ * @type {Schema}
+ */
+var FavoriteTopicSchema = new Schema({
+  userId: {
+    type: String,
+    required: true
+  },
+  topicId: {
+    type: String,
+    required: true
+  }
+}, {
+  collection: 'favorite_topic'
+});
+
+/**
+ * Plugins
+ */
+FavoriteTopicSchema
+  .plugin(require('../mongoose_plugins/timestamp'));
+
+/**
+ * Exports schema and model name
+ * @type {object}
+ */
+module.exports = {
+  schema: FavoriteTopicSchema,
+  modelName: 'FavoriteTopic'
+};

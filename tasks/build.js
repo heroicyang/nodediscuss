7344c511caba314a146880e913c03fa3567a55ab
HeroicYang <me@heroicyang.com>
2013-11-07 22:27:27 +0800

    修改了构建任务的逻辑

--- tasks/build.js
+++ tasks/build.js
@@ -14,22 +14,34 @@ module.exports = exports = function(grunt) {
       dest = grunt.option('dest') || 'assets';
     dest = path.join(process.cwd(), dest);
 
+    grunt.config('dest', dest);
+    grunt.task.run('clean');
+
     switch(target) {
+      case 'pro':
+      case 'production':
+      case 'release':
+        grunt.log.writeln('Building client...');
+        break;
       case 'dev':
       case 'development':
       default:
-        grunt.config('dest', dest);
-        grunt.task.run('clean');
         grunt.task.run('stylus:development');
         grunt.task.run('concat:development');
         grunt.task.run('copy');
         grunt.task.run('watch:build');
+        watching();
         break;
     }
+  });
 
+  function watching() {
     // 响应 `grunt-contrib-watch` 插件的 `watch` 事件，并根据...
     // ...变动的文件来运行相应的任务
-    grunt.event.on('watch', function(action, filepath) {
+    grunt.event.on('watch', function(action, filepath, target) {
+      if (target !== 'build') {
+        return;
+      }
       if (filepath.indexOf('client/styl') !== -1) {
         grunt.task.run('clean:stylus');
         grunt.task.run('stylus:development');
@@ -40,5 +52,5 @@ module.exports = exports = function(grunt) {
         grunt.task.run('copy:js');
       }
     });
-  });
+  }
 };

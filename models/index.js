335cd53cedace4ba0699d7d12eb07a7f9bd9b299
HeroicYang <me@heroicyang.com>
2013-11-08 15:09:54 +0800

    在 model 中增加常量定义

--- models/index.js
+++ models/index.js
@@ -18,6 +18,21 @@ if (!mongoose.modelNames().length) {
 
 module.exports = exports = mongoose.models;
 
+/**
+ * 定义 model 中会用到的常量，并将其暴露出去
+ * @type {Object}
+ */
+exports.constants = {};
+defineConstant(exports.constants, {
+  // 通知类型
+  NITIFICATION_TYPE: {
+    COMMENT: 1,         // 某人评论了你的话题
+    REPLY_COMMENT: 2,   // 某人回复了你的评论
+    FOLLOW: 3,          // 某人关注了你
+    AT: 4               // 某人@了你
+  }
+});
+
 function registerModels() {
   var schemaPath = getFullPath('./schemas/');
 
@@ -65,4 +80,33 @@ function registerModels() {
 
 function getFullPath(p) {
   return join(__dirname, p);
+}
+
+/**
+ * 定义常量，真正意义上的常量，只可读不可写
+ * @param  {Object} object  用于绑定常量的对象
+ * @param  {String} name    常量名称
+ * @param  {[type]} value   常量值
+ * @return {Object}         绑定常量的对象
+ */
+function defineConstant(object, name, value) {
+  var key;
+
+  // 如果传入的第二个参数为一个对象，则遍历该对象进行常量定义
+  if (typeof name === 'object') {
+    for (key in name) {
+      if (name.hasOwnProperty(key)) {
+        defineConstant(object, key, name[key]);
+      }
+    }
+  } else {
+    Object.defineProperty(object, name, {
+      value:        value,
+      enumerable:   true,
+      writable:     false,
+      configurable: false
+    });
+  }
+
+  return object;
 }

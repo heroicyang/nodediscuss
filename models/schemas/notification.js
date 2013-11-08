740515eddf40396f3c775e15708ff0e195effbfa
HeroicYang <me@heroicyang.com>
2013-11-08 15:27:19 +0800

    增加了 relation 和 notification 的 schema 定义

---
+++ models/schemas/notification.js
@@ -0,0 +1,53 @@
+/**
+ * NotificationSchema definition
+ * @author heroic
+ */
+
+/**
+ * Module dependencies
+ */
+var mongoose = require('mongoose'),
+  Schema = mongoose.Schema,
+  _ = require('lodash'),
+  constants = require('../constants');
+
+/**
+ * Collection name in the database is `notification`
+ * @type {Schema}
+ */
+var NotificationSchema = new Schema({
+  masterId: {
+    type: String,
+    required: true,
+    index: true
+  },
+  userId: String,
+  type: {
+    type: Number,
+    required: true,
+    enum: _.values(constants.NITIFICATION_TYPE)
+  },
+  topicId: String,
+  commentId: String,
+  hasRead: {
+    type: Boolean,
+    default: false
+  }
+}, {
+  collection: 'notification'
+});
+
+/**
+ * Plugins
+ */
+NotificationSchema
+  .plugin(require('../mongoose_plugins/timestamp'));
+
+/**
+ * Exports schema and model name
+ * @type {object}
+ */
+module.exports = {
+  schema: NotificationSchema,
+  modelName: 'Notification'
+};
---
+++ models/schemas/relation.js
@@ -0,0 +1,44 @@
+/**
+ * RelationSchema definition
+ * @author  heroic
+ */
+
+/**
+ * Module dependencies
+ */
+var mongoose = require('mongoose'),
+  Schema = mongoose.Schema;
+
+/**
+ * Collection name in the database is `relation`
+ * @type {Schema}
+ */
+var RelationSchema = new Schema({
+  userId: {
+    type: String,
+    required: true,
+    index: true
+  },
+  followId: {
+    type: String,
+    required: true,
+    index: true
+  }
+}, {
+  collection: 'relation'
+});
+
+/**
+ * Plugins
+ */
+RelationSchema
+  .plugin(require('../mongoose_plugins/timestamp'));
+
+/**
+ * Exports schema and model name
+ * @type {object}
+ */
+module.exports = {
+  schema: RelationSchema,
+  modelName: 'Relation'
+};

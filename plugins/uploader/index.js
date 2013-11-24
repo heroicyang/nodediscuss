827c5d631457b9e5c9803cc2c98826c2c577d646
HeroicYang <me@heroicyang.com>
2013-11-24 16:13:00 +0800

    统一文件上传策略中的处理逻辑

--- plugins/uploader/index.js
+++ plugins/uploader/index.js
@@ -74,12 +74,10 @@ Uploader.prototype.unuse = function(name) {
  * @param  {String}   strategy  文件上传策略名称
  * @param  {Object|Array}   files    文件对象或文件对象数组
  * @param  {Function} callback  回调函数
- *  - err          如果是单一文件上传，只要上传出错，该 err 即代表错误对象；
- *                 如果是批量文件上传，这个值总是为 null ，因为此时不应该打断后面的文件上传。
- *  - file|files   文件名变更之后的文件对象或文件数组
- *  - errors       本次上传的文件中出错的文件及其错误信息。如果是上传单一文件，则该值始终为 null
- *    - filename      原来的文件名
- *    - err           具体的出错原因
+ *  - err    这个值基本上总是为 null ，因为文件上传的错误应该收集起来返回给客户端，...
+ *            ...而且也不应该打断后面的文件上传。
+ *  - uploadedFiles    上传成功的文件对象或文件数组
+ *  - failedFiles    上传失败的文件对象或文件数组
  */
 Uploader.prototype.upload = function(strategy, files, callback) {
   var name = strategy;
@@ -93,7 +91,8 @@ Uploader.prototype.upload = function(strategy, files, callback) {
   }
 
   var fileCount = files.length,
-    errors = {};
+    uploadedFiles = [],
+    failedFiles = [];
   files.forEach(function(file) {
     var uid = uuid.v1(),
       fileExt = path.extname(file.name);
@@ -113,25 +112,17 @@ Uploader.prototype.upload = function(strategy, files, callback) {
     // 使用对应的上传策略上传图片
     strategy.upload(file, function(err) {
       if (err) {
-        // 此时需要再判断一下，如果本次只上传一个文件，且已经出错了
-        // 则直接调用回调函数，上面的 forEach 本身也没有数据继续往下执行了
-        if (fileCount === 1) {
-          return callbackWrap(err);
-        }
-        // 否则只做错误收集，然后继续上传后面的文件
-        errors[file._name] = err;
+        // 只做错误收集，然后继续上传后面的文件
+        file.error = err;
+        failedFiles.push(file);
+      } else {
+        uploadedFiles.push(file);
       }
 
-      // 此时还要再判断一下，如果本次只上传一个文件，而且也没有出错
-      // 那也直接调用回调函数，告诉其上传成功
-      if (fileCount === 1) {
-        return callbackWrap(null, file);
-      }
-
-      // 如果是批量文件上传，不管出错与否，都得等到计数器变为 0 才去调用回调函数
       fileCount -= 1;
+      // 等到计数器变为 0 才去调用回调函数，代表所有文件都已上传
       if (fileCount === 0) {
-        callbackWrap(null, files, errors);
+        callbackWrap(null, uploadedFiles, failedFiles);
       }
     });
   });

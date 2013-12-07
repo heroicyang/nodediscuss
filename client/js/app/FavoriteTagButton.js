a4af6abd9d0c31ce5043353b88340f87617168f1
HeroicYang <me@heroicyang.com>
2013-12-07 22:43:01 +0800

    修复不能收藏节点的BUG

--- client/js/app/FavoriteTagButton.js
+++ client/js/app/FavoriteTagButton.js
@@ -8,23 +8,22 @@ NC.Module.define('FavoriteTagButton', [], function() {
       var self = this,
         url;
       if (this.data.isFavorited) {
-        url = '/tag/' + this.data.id + '/del_favorite';
+        url = '/tag/' + this.data.name + '/unfavorite';
       } else {
-        url = '/tag/' + this.data.id + '/favorite';
+        url = '/tag/' + this.data.name + '/favorite';
       }
 
-      $.post(url, { name: this.data.name })
+      $.post(url)
         .done(function(data) {
           if (data.success) {
+            self.data.isFavorited = !self.data.isFavorited;
             if (self.data.isFavorited) {
-              self.$el.attr('title', '收藏' + self.data.name + '节点');
-              self.$el.text('收藏该节点');
-            } else {
               self.$el.attr('title', '取消收藏' + self.data.name + '节点');
               self.$el.text('取消收藏');
+            } else {
+              self.$el.attr('title', '收藏' + self.data.name + '节点');
+              self.$el.text('收藏该节点');
             }
-
-            self.data.isFavorited = !self.data.isFavorited;
           }
         });
     }

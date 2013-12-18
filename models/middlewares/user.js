ce80a76c5beae7808c320291916283d84bd6da4e
HeroicYang <me@heroicyang.com>
2013-12-18 17:22:59 +0800

    user middleware 新增了更新昵称时的处理逻辑

--- models/middlewares/user.js
+++ models/middlewares/user.js
@@ -6,7 +6,7 @@
 /**
  * Module dependencies
  */
-var sanitize = require('../../sanitize');
+var sanitize = require('../sanitize');
 
 module.exports = exports = function(schema) {
   // 执行数据验证之前的中间件
@@ -23,5 +23,40 @@ module.exports = exports = function(schema) {
         this.nickname = this.username;
       }
       next();
+    });
+
+  // 执行数据保存操作之前的中间件
+  schema
+    .pre('save', true, function(next, done) {
+      next();
+      // 如果更新了用户的昵称，则同步更新其话题的 author 副本信息
+      if (this.isNew || !this.isModified('nickname')) {
+        return done();
+      }
+
+      var Topic = this.model('Topic');
+      Topic.update({
+        'author.id': this.id
+      }, {
+        'author.nickname': this.nickname
+      }, {
+        multi: true
+      }, done);
+    })
+    .pre('save', true, function(next, done) {
+      next();
+      // 如果更新了用户的昵称，则同步更新其评论的 author 副本信息
+      if (this.isNew || !this.isModified('nickname')) {
+        return done();
+      }
+      
+      var Comment = this.model('Comment');
+      Comment.update({
+        'author.id': this.id
+      }, {
+        'author.nickname': this.nickname
+      }, {
+        multi: true
+      }, done);
     });
 };

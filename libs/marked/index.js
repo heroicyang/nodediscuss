d00063dd8c1775c03651d1bade67ba41c5616c42
HeroicYang <me@heroicyang.com>
2013-12-20 15:23:40 +0800

    增加 marked 库的 api fallback 特性

--- libs/marked/index.js
+++ libs/marked/index.js
@@ -11,39 +11,55 @@
 var marked = require('marked'),
   hljs = require('highlight.js');
 
-// 使用 `highlight.js` 库来解析代码块
-var r = new marked.Renderer();
-r.code = function(code, lang) {
-  if (lang) {
-    return hljs.highlight(lang, code).value;
-  } else {
-    return hljs.highlightAuto(code).value;
-  }
-};
-
-// 对外部链接加上 `target="_blank"` 的特性
-r.link = function(href, title, text) {
-  var out = '<a href="' + href + '"';
-  if (title) {
-    out += ' title="' + title + '"';
-  }
-  if (/(https?|s?ftp|git)/i.test(href)) {
-    out += ' target="_blank"';
-  }
-  out += '>' + text + '</a>';
-  return out;
-};
-
-marked.setOptions({
+var options = {
   gfm: true,
   tables: true,
   breaks: true,
   pedantic: false,
   sanitize: true,
   smartLists: true,
-  smartypants: false,
-  renderer: r
-});
+  smartypants: false
+};
+
+if (marked.Renderer) {
+  // 新版 API，已在 GitHub 的 master 分支更新，但未发布到 npm
+  var r = new marked.Renderer();
+
+  // 使用 `highlight.js` 库来解析代码块
+  r.code = function(code, lang) {
+    if (lang) {
+      return hljs.highlight(lang, code).value;
+    } else {
+      return hljs.highlightAuto(code).value;
+    }
+  };
+
+  // 对外部链接加上 `target="_blank"` 的特性
+  r.link = function(href, title, text) {
+    var out = '<a href="' + href + '"';
+    if (title) {
+      out += ' title="' + title + '"';
+    }
+    if (/(https?|s?ftp|git)/i.test(href)) {
+      out += ' target="_blank"';
+    }
+    out += '>' + text + '</a>';
+    return out;
+  };
+
+  options.renderer = r;
+} else {      // fallback
+  // 使用 `highlight.js` 库来解析代码块
+  options.highlight = function(code, lang) {
+    if (lang) {
+      return hljs.highlight(lang, code).value;
+    } else {
+      return hljs.highlightAuto(code).value;
+    }
+  };
+}
+
+marked.setOptions(options);
 
 /**
  * Expose marked
